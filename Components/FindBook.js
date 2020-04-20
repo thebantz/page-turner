21374e337c329d84b54326d18d013b1967f466fb
Nicole <nicoledbanta@gmail.com>
2020-04-20 13:20:53 -0700

    Fetch sample data

--- Components/FindBook.js
+++ Components/FindBook.js
@@ -1,11 +1,32 @@
 import * as React from 'react';
-import { useState } from "react";
-import { Platform, StyleSheet, Text, View, Button, Alert, TouchableOpacity, Image } from 'react-native';
+import { useState, useEffect } from "react";
+import { FlatList, ActivityIndicator, Platform, StyleSheet, Text, View, Button, Alert, TouchableOpacity, Image } from 'react-native';
 
 function FindBook({ navigation }) {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://reactnative.dev/movies.json')
+      .then((response) => response.json())
+      .then((json) => setData(json.movies))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+
+  })
+
   return (
     <View style={styles.container}>
       <Text>Find Book Dropdown Place Here</Text>
+      {isLoading ? <ActivityIndicator /> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.title}, {item.releaseYear}</Text>
+          )}
+        />
+      )}
     </View>
   )
 }
