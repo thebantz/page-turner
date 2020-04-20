e34d8b695e2376323425c3ba114c7b11e2cda436
Nicole <nicoledbanta@gmail.com>
2020-04-19 21:53:18 -0700

    Render Home Screen

--- App.js
+++ App.js
@@ -1,18 +1,61 @@
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+
+import HomeScreen from './Components/HomeScreen.js';
+
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
   android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
 });
 
+const Stack = createStackNavigator();
+
+
+function TestScreen({ navigation }) {
+  return (
+    <View style={{
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      ></Button>
+    </View>
+  )
+}
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to HomeScreen"
+        onPress={() => navigation.navigate('Home')}
+      />
+    </View>
+  );
+}
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" options={{ headerShown: false }}
+          component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # page-turner
+## A reading habit builder application
